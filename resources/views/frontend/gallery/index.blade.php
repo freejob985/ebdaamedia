04a52beaf6bd18871ecf4b87d459d4a5509aa51d
freejob985 <freejob985@gmail.com>
2021-02-05 00:04:39 +0200

    asd

--- resources/views/frontend/gallery/index.blade.php
+++ resources/views/frontend/gallery/index.blade.php
@@ -34,7 +34,7 @@
                                       @if ($gallery->Type=="picture")
                                       <figure class="image-box">
                                             <img style="width:100%;height:280px;" src="{{ asset('uploads/img/galleries/'.$gallery->gallery_image) }}" alt="blog image">
-                                            <a href="{{ asset('uploads/img/galleries/'.$gallery->gallery_image) }}" class="lightbox-image" data-fancybox="gallery"><i class="flaticon-zoom"></i></a>
+                                            <a  class="lightbox-image" data-fancybox="gallery"><i class="flaticon-zoom"></i></a>
                                         </figure>
                                         
                                   @elseif  ($gallery->Type=="youtube")      
